330affa5d56dbc2b2aaf8db0c3dbb98cba623cb8
Turbo Thinh (NoA Ignite) <me@turboninh.com>
2023-05-26 01:33:58 +0200

    initial commit

---
+++ components/Image/Image.stories.tsx
@@ -0,0 +1,42 @@
+import type {Meta, StoryObj} from "@storybook/react";
+import {waitFor} from "@storybook/testing-library";
+import Image, {ImageProps} from "next/image";
+import {waitForImagesToLoad} from "utils";
+
+const meta = {
+  title: "Display/Image",
+  component: Image
+} satisfies Meta<ImageProps>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+/** Default Avatar variant */
+export const Default = {
+  args: {
+    src: "https://powered-by.qbank.se/dm-umbraco/png_3840_1:1/Andersen-martini_s%C3%B8borg_38.png",
+    alt: "Andersen-martini_søborg_38",
+    width: 500,
+    height: 500
+  },
+  play: async () => {
+    await waitFor(waitForImagesToLoad, {timeout: 5000});
+  },
+  parameters: {
+    chromatic: {
+      viewports: [375, 640, 960, 1280, 1440]
+    }
+  },
+  render: (args) => {
+    return (
+      <div style={{width: "100%", height: "100%", position: "relative"}}>
+        <Image {...args} />
+        <Image {...args} />
+        <Image {...args} />
+        <Image {...args} />
+        <Image {...args} />
+      </div>
+    );
+  }
+} satisfies Story;
